e1b2d7d928d629ba763c2939d550ba85e5317ab8
Alissonn-XX <costaalisson777@gmial.com>
2023-06-21 20:27:20 -0300

    alteracao

--- js/index.js
+++ js/index.js
@@ -1,19 +1,20 @@
 const btnEnviar = document.querySelector('#enviar');
 const btnProximo = document.querySelector('#proximo');
-let  quiz = document.querySelectorAll('input[type=radio]');
+let quiz = document.querySelectorAll('input[type=radio]');
+let opcoes = document.querySelectorAll('.opcao')
 let quizEnunciado = document.querySelector('#conteiner-item h5')
-
-
-
+let controle = 0;
 let pontuacao;
-const perguntasQuiz = [
- {pergunta: 'Em Quantas nações Naruto é divido?', resposta: 5},
+
+
+const perguntasQuiz=[
+ {pergunta: 'Em Quantas nações Naruto é divido?',  resposta: 5},
  {pergunta: 'Quais os nomes das nações como ninja?', resposta: 'Terra, Vento, fogo, Trovão e Água'},
  {pergunta: 'Quem selou a raposa no corpo do Naruto?', resposta: '4 Hokaque'},
  {pergunta: 'Qual ninja tem habilidades de estrategista?', resposta: 'Shikamaru'},
  {pergunta: 'Qual é o nome da organização que usa capa preta?', resposta: 'Akatisuki'},
  {pergunta: 'Qual o nome da pessoa que tem uma besta selada no corpo?', resposta: 'Jinchuuriki'}
-];
+]
 
 const alternativas = [
   {um: 2, dois: 8, tres: 2},
@@ -21,20 +22,20 @@ const alternativas = [
   {um: '3 Hokaque', dois: '4 Hokaque', tres: '7 Hokaque'},
   {um: 'Rock Lee', dois: 'Naruto', tres: 'Shikamaru'},
   {um: 'Akatisuki', dois: 'Ninjas Renegados', tres: 'Aldeia do Fogo'},
-  {um: 'Ninja', dois: 'Jinchuuriki', tres: 'Besta'},
+  {um: 'Ninja', dois: 'Jinchuuriki', tres: 'Besta'}
 ];
+quizEnunciado.innerText = perguntasQuiz[controle].pergunta;
 
-let controle = 0;
-/* while(controle >= perguntasQuiz.length){
-  controle++
-} */
-quizEnunciado.innerText = perguntasQuiz[controle].pergunta
+let obj = {a: 1, b: 2}
+let {a,b} = obj
+console.log(a,b);
 
-
-
-quiz.forEach((item)=>{
-   item.innerText = alternativas[0].um
-})
+const alternativa =(respostasOp = [])=>{
+ let controle = 0
+  respostasOp[controle].innerText = 'ola'
+  respostasOp[controle + 1].innerText = 'ola'
+  respostasOp[controle + 2].innerText = 'ola'
+}
 
 
 
@@ -44,14 +45,8 @@ btnEnviar.addEventListener('click',()=>{
 });
 
 btnProximo.addEventListener('click',()=>{
-    if(controle > perguntasQuiz.length){
-        controle = 0
-        return
-    }else{
-        controle++;
-        quizEnunciado.innerText = perguntasQuiz[controle].pergunta
-    }
-
-    console.log(controle);
+    
+    
+    alternativa(opcoes)
 })
 
